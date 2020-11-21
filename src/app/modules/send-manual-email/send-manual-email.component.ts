0c187cf37292a72aa8d72bcd541c971880f5e31d
kamleshOnGit <master.kamlesh@outlook.com>
2020-11-21 16:16:51 +0530

    email sending functionality done

--- src/app/modules/send-manual-email/send-manual-email.component.ts
+++ src/app/modules/send-manual-email/send-manual-email.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+declare let Email;
 
 @Component({
   selector: 'app-send-manual-email',
@@ -12,7 +13,25 @@ export class SendManualEmailComponent implements OnInit {
 
   ngOnInit() {
   }
-  
+
+  onSubmit(f: NgForm) {
+
+    Email.send({
+      SecureToken : 'C973D7AD-F097-4B95-91F4-40ABC5567812',
+      To : 'them@website.com',
+      From : 'you@isp.com',
+      Subject : 'his is the subject',
+      Body : 'And this is the body',
+    Attachments : [
+    {
+      name : 'mtpjs.png',
+      path : 'https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png'
+    }]
+  }).then(
+    message => alert(message)
+  );
+
+    }
 
 
 }
