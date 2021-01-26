1963b117980e572702848cff103d6a6daedd82fd
shubham sharma <sheshank.jamwal@debugged-pro.com>
2021-01-26 10:42:14 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { version } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'dashboard';
   
+  constructor(){
+    console.log("version(0.1)");
+  }
 }
